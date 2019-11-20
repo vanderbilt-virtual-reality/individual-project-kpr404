a146a6e81e357baa5f74511edcd1d2a4a08045d9
Kelson Reiss <kelson.reiss@gmail.com>
2019-11-20 11:26:23 -0600

    Added Scenes to Unity

---
+++ LoadSceneOnClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+
+    private readonly static int FLOOD_SCENE_INDEX = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+
+        // If both joystick buttons are pressed, advance scene
+        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstick) && OVRInput.Get(OVRInput.Button.SecondaryThumbstick))
+        {
+            SceneManager.LoadScene(FLOOD_SCENE_INDEX);
+        }
+    }
+}
